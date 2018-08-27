867bc26996f68e24bd496d1bf15841708653537c
DESKTOP-63COLD0\Excse <tbehrend@yahoo.com>
2018-08-27 11:59:38 +0200

    Hello World

---
+++ src/eu/aragonapp/server/Main.java
@@ -0,0 +1,18 @@
+package eu.aragonapp.server;
+
+/**
+ * @Copyright (c) 2018 Mythic Inc. (http://www.mythic.com/) All Rights Reserved.
+ * <p>
+ * Mythic Inc. licenses this file to you under the Apache License,
+ * @Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * @https://www.apache.org/licenses/LICENSE-2.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
